93ea01edb4b6f5930a40ec3ac7e6ba0e58d34a4c
LucasH15 <hello@lucas-hubert.me>
2022-09-17 15:00:43 +0200

    add light schemas

---
+++ backend_sanity/schemas/articles.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'articles',
+  title: 'Articles',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    }
+  ]
+}
